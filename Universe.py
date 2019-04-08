b5577dfe7317164653f11a0065c97b08afcfc0e3
0x10F8 <cgardi203@caledonian.ac.uk>
2019-04-08 23:48:41 +0100

    Updated file names

--- Universe.py
+++ Universe.py
@@ -1,20 +1,20 @@
-import Api
+import api
 
 
 def get_region_ids():
-    return Api.do_get(1, 'universe/regions', {})
+    return api.do_get(1, 'universe/regions', {})
 
 
 def get_region_info(id):
-    return Api.do_get(1, 'universe/regions/' + str(id), {})
+    return api.do_get(1, 'universe/regions/' + str(id), {})
 
 
 def get_system_ids():
-    return Api.do_get(1, 'universe/systems', {})
+    return api.do_get(1, 'universe/systems', {})
 
 
 def get_system_info(id):
-    return Api.do_get(4, 'universe/systems/' + str(id), {})
+    return api.do_get(4, 'universe/systems/' + str(id), {})
 
 
 for id in get_system_ids():
