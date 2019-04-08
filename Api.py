eeae10a0bbb19f270e6285b2ca67b05127483b02
0x10F8 <cgardi203@caledonian.ac.uk>
2019-04-08 23:42:37 +0100

    Updated API calls to do retries

--- Api.py
+++ Api.py
@@ -2,7 +2,7 @@ import requests
 
 DATASOURCE = 'tranquility'
 BASE_URL = 'https://esi.evetech.net'
-RETRIES = 4
+RETRIES = 10
 
 
 def build_url(version, path):
@@ -14,10 +14,11 @@ def do_get(version, path, params):
 
 
 def do_get_retry(version, path, params, retries_remaining):
-    url = build_url(version, path)
-    params['datasource'] = DATASOURCE
-    try:
-        response = requests.get(url=url, params=params).json()
-    except:
-        response = do_get_retry(version, path, params, (retries_remaining - 1))
-    return response
+    if retries_remaining > 0:
+        url = build_url(version, path)
+        params['datasource'] = DATASOURCE
+        try:
+            response = requests.get(url=url, params=params).json()
+        except:
+            response = do_get_retry(version, path, params, (retries_remaining - 1))
+        return response
